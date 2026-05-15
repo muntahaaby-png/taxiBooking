6eb29f6d6b8508e8a0408baf93e38a942f125121
nadahub-hue <16s196@utas.edu.om>
2026-05-15 23:39:52 +0400

    Delete taxi-client directory

--- taxi-client/src/components/_test_/Home.test.js
+++
@@ -1,7 +0,0 @@
-import Home from "../Home"
-import {render} from "@testing-library/react"
-
-test("To test the UI of Home.js", () => {
-    const {container} = render(<Home />)
-    expect(container).toMatchSnapshot()
-})
